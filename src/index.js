168f7cb2b35bac1cd3567ff27ca809391b8a3637
zaxanq <zaxanq@gmail.com>
2020-04-16 18:05:41 +0200

    Header finished for desktop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles/style.scss';
+import Logo from './assets/images/logo-zonex.svg';
+
+const renderLogo = () => {
+  document.querySelector('.header__logo').src = Logo;
+};
+
+renderLogo();
